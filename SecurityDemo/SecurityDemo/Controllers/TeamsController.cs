62ff20a55d2a9a7a1673c99d9a34410c72754e56
Hans Peeters <hspeeters@gmail.com>
2016-03-10 22:27:58 +0100

    Simple api controller

---
+++ SecurityDemo/SecurityDemo/Controllers/TeamsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SecurityDemo.Controllers
+{
+    public class TeamsController : ApiController
+    {
+        [Route("api/teams")]
+        public IHttpActionResult Get()
+        {
+            return Ok(new Team
+            {
+                Id = 1,
+                Name = "Lierse",
+                Players = new List<Player>
+                {
+                    new Player {Id = 1, FirstName = "Jos", LastName = "De vos"}
+                }
+            });
+        }
+    }
+
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Player> Players  { get; set; }
+    }
+
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Team Team { get; set; }
+    }
+}

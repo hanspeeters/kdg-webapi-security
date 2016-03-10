e8045b49eaa20c05f21c18642543233b56ebbca4
Hans Peeters <hspeeters@gmail.com>
2016-03-10 23:40:44 +0100

    Added ef and first migration

---
+++ SecurityDemo/SecurityDemo/Migrations/201603102223206_First.cs
@@ -0,0 +1,42 @@
+namespace SecurityDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class First : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Players",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Team_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Teams", t => t.Team_Id)
+                .Index(t => t.Team_Id);
+            
+            CreateTable(
+                "dbo.Teams",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Players", "Team_Id", "dbo.Teams");
+            DropIndex("dbo.Players", new[] { "Team_Id" });
+            DropTable("dbo.Teams");
+            DropTable("dbo.Players");
+        }
+    }
+}
---
+++ SecurityDemo/SecurityDemo/TeamsContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using SecurityDemo.Controllers;
+
+namespace SecurityDemo
+{
+    public class TeamsContext : DbContext
+    {
+        public TeamsContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public IDbSet<Team> Teams { get; set; }
+        public IDbSet<Player> Players { get; set; }
+    }
+}
